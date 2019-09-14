16befed20164fef74e59e0594dfbdf29ffc459dc
Marc Anton Dahmen <mail@marcdahmen.de>
2019-09-14 16:15:53 +0200

    Minor fixes

--- snippets/footer.php
+++ snippets/footer.php
@@ -33,6 +33,7 @@
 		</footer>
 	</div>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/highlight.min.js"></script>
+	<script>hljs.initHighlightingOnLoad();</script>
 	@{ itemsFooter }
 </body>
 </html>
