79cad6134ec8697401e45f2c446d3e7d36b6deaf
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-24 11:40:46 +0200

    feat: use new meta tags engine in Automad 2

--- snippets/header.php
+++ snippets/header.php
@@ -2,17 +2,19 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-	<@ set {
-		metaTitle: "@{ sitename } / @{ title | def ('404') }"
-	} @>
-    <title>@{ metaTitle | stripTags }</title>
-	<@ Automad/MetaTags {
-		description: @{ textTeaser | stripTags },
-		ogTitle: @{ metaTitle | stripTags },
-		ogDescription: @{ textTeaser | stripTags },
-		ogImage: @{ ogImage },
-		twitterCard: 'summary_large_image'
-	} @>
+	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
+		<@ set {
+			metaTitle: "@{ sitename } / @{ title | def ('404') }"
+		} @>
+		<title>@{ metaTitle | stripTags }</title>
+		<@ Automad/MetaTags {
+			description: @{ textTeaser | stripTags },
+			ogTitle: @{ metaTitle | stripTags },
+			ogDescription: @{ textTeaser | stripTags },
+			ogImage: @{ ogImage },
+			twitterCard: 'summary_large_image'
+		} @>
+	<?php } ?>
 	<@ with @{ favicon } @><link href="@{ :file }" rel="shortcut icon" type="image/x-icon" /><@ end @>
 	<@ with @{ appleTouchIcon } @><link href="@{ :file }" rel="apple-touch-icon" /><@ end @>
 	<link rel="stylesheet" href="/packages/@{ theme }/dist/normalize/normalize.css">
